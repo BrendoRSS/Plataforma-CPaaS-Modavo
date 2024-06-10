7143df45b912a64e72860fd5b7d9140660cb4e75
Hebertsfj <hebertsdefreitas@gmail.com>
2024-06-10 15:04:32 -0300

    acerto validação cpf

--- assets/js/validacao-cadastro.js
+++ assets/js/validacao-cadastro.js
@@ -131,40 +131,47 @@ function ValidaCpf(cpf) {
 
     cpf = cpf.replace(/\.|-/g, "");
 
-    soma = 0;
-    soma += cpf[0] * 10;
-    soma += cpf[1] * 9;
-    soma += cpf[2] * 8;
-    soma += cpf[3] * 7;
-    soma += cpf[4] * 6;
-    soma += cpf[5] * 5;
-    soma += cpf[6] * 4;
-    soma += cpf[7] * 3;
-    soma += cpf[8] * 2;
-    soma = (soma * 10) % 11;
-    if (soma == 10 || soma == 11)
-        soma = 0;
-    if (soma != cpf[9])
-        return false;
+  
+     soma = 0;
+     soma += cpf[0] * 10;
+     soma += cpf[1] * 9;
+     soma += cpf[2] * 8;
+     soma += cpf[3] * 7;
+     soma += cpf[4] * 6;
+     soma += cpf[5] * 5;
+     soma += cpf[6] * 4;
+     soma += cpf[7] * 3;
+     soma += cpf[8] * 2;
+     valor = soma % 11;
+     if (valor <=1){
+        digito = 0;
+     } else{
+        digito = 11 - valor;
+     }
+        
+     if (digito != cpf[9])
+         return false;
 
-    soma = 0;
-    soma += cpf[0] * 11;
-    soma += cpf[1] * 10;
-    soma += cpf[2] * 9;
-    soma += cpf[3] * 8;
-    soma += cpf[4] * 7;
-    soma += cpf[5] * 6;
-    soma += cpf[6] * 5;
-    soma += cpf[7] * 4;
-    soma += cpf[8] * 3;
-    soma += cpf[9] * 2;
-    soma = (soma * 10) % 11;
-    if (soma == 10 || soma == 11) {
-        soma = 0;
-    }
+     soma2 = 0;
+     soma2 += cpf[0] * 11;
+     soma2 += cpf[1] * 10;
+     soma2 += cpf[2] * 9;
+     soma2 += cpf[3] * 8;
+     soma2 += cpf[4] * 7;
+     soma2 += cpf[5] * 6;
+     soma2 += cpf[6] * 5;
+     soma2 += cpf[7] * 4;
+     soma2 += cpf[8] * 3;
+     soma2 += cpf[9] * 2;
+     valo2 = soma2 % 11;
+     if (valor2 <= 1) {
+         digito2 = 0;
+     }else{
+        digito2 = 11 - valor;
+     }
 
-    if (soma != cpf[10]) {
-        return false
+     if (digito2 != cpf[10]) {
+      return false
     }
 
 }
